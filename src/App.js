5944323a32a3c086bb6ef690d5c83468dfedeae3
BoubakerSaif <boubakersaif.contact@gmail.com>
2023-11-15 20:28:58 +0100

    Timer

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import "./App.css";
+import Profile from "./Components/Profile";
+import Timer from "./Components/Timer";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      fullName: "Saif Boubaker",
+      Bio: "Born in 1996",
+      imageSrc:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGrXB8jM7Sdicev8vsGSDFBgPb9ABbyFJDkscCKHjnGoMbrdi7-nXxMDJAx-QyxwGYlQQ&usqp=CAU",
+      Profession: "Developer",
+      show: false,
+    };
+  }
+  showProfile = () => {
+    this.setState({ show: !this.state.show });
+  };
+  render() {
+    return (
+      <div className="App">
+        {this.state.show && (
+          <Profile
+            fullName={this.state.fullName}
+            desc={`${this.state.Bio}, and he is a ${this.state.Profession}`}
+            ProfileImage={this.state.imageSrc}
+          />
+        )}
+
+        <button onClick={this.showProfile}>
+          {this.state.show ? "Hide Profile" : "Show Profile"}
+        </button>
+        <div style={{ marginTop: "70px" }}>
+          <Timer />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
